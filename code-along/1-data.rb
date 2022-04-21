caf05390c462b9cc31b2004d80b662e806b2cc1f
tori-goodell <tori.goodell@kellogg.northwestern.edu>
2022-04-21 00:20:47 +0000

    learned fundamentals of ruby

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,39 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
-
+puts 5
 # Perform simple math with numbers
+puts 5+3
+puts 5-3
+puts 5*3
+puts 5/3
+puts 5%3
+puts 5.0 / 3.0
 
 # Strings
+puts "Hello, world"
 
 # Combine strings together
+puts "Hello," + "world"
+puts "Tacos" * 3
+puts "Tacos" + 3.to_s
 
 # Variables
+a = 5
+b = 3
+puts a
+
+food = "tacos"
+quantity = 3
+puts food + ":" + quantity.to_s #tacos:3
 
 # Combine strings and variables
+puts "#{food}: #{quantity}"
+first_name = "Mando"
+puts "Hello, #{first_name}"
 
 # String manipulation
+puts "Hello".reverse
+puts "Hello".length
+puts "Hello".upcase
+puts "Hello".downcase
--- labs/1-data.rb
+++ labs/1-data.rb
@@ -15,3 +15,12 @@
 # rand(1..6)
 # Log output to the screen with puts:
 # puts "Hello, world"
+
+first_roll = rand(1..6)
+# puts first_roll
+
+second_roll = rand(1..6)
+# puts second_roll
+
+total = first_roll + second_roll
+puts "Your rolled #{total}"
